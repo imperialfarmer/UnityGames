03614a8c101bc56943a870057bffc720519c5d62
imperialfarmer <yin062398@gmail.com>
2018-05-13 23:49:02 +0100

    Start the project "Glitch Garden"

---
+++ GlitchGraden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+// lead the game to the right scene
+	private static int currentLevel = 1;
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		if(autoLoadNextLevelAfter <= 0f){
+			
+		}else{
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLever(string name){
+		//Debug.Log("Lever request for : " + name);
+		// jump to the scene which has been input in gui
+		// "Game" is given, so the after click this button
+		// the game will start the scene "Game" 
+		Application.LoadLevel(name);
+		//Brick.breakableCount = 0;
+	}
+
+	public void QuitRequest(){
+		//Debug.Log("I wanna quit!");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel(){
+		// automatically load the next level
+		//Brick.breakableCount = 0;
+		currentLevel++;
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void loadCurrentLevel(){
+		Application.LoadLevel(currentLevel);
+	}
+
+	public void BrickDestoyed(){
+		// if this is the last brick, then load next level
+		//if(Brick.breakableCount <= 0){
+		//	LoadNextLevel();
+		//}
+	}
+}
---
+++ GlitchGraden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+
+	void Update () {
+		
+	}
+
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log("in Level " + level);
+		if(thisLevelMusic){ // if there is music attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
