ba7c137f14ecf765f5cbeba5519fdd893ebec789
imperialfarmer <yin062398@gmail.com>
2018-05-21 21:34:09 +0100

    Start the project "BowlMaster"

---
+++ BowlMaster/Assets/BowlingBall/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed;
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        BallLaunch();
+    }
+
+    public void BallLaunch()
+    {
+        rigidBody.velocity = new Vector3(0f, 0f, launchSpeed);
+        audioSource.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
