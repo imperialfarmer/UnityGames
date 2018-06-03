b85c7e3b108884077ea753af028b27839d7802db
imperialfarmer <yin062398@gmail.com>
2018-06-03 23:51:02 +0100

    framwork of Rocket finished

---
+++ Advanced/RocketAdventure/Assets/Player/Rocket/Rocket.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float thrustPowerPerFrame = 50f;
+    [SerializeField] float rotatePowerPerFrame = 300f;
+    [SerializeField] float transitionTime = 1f;
+
+    public static int currentLevel = 1;
+    [SerializeField] int damage = 25;
+
+    [SerializeField] ParticleSystem enginePS;
+    [SerializeField] ParticleSystem successPS;
+    [SerializeField] ParticleSystem damagedPS;
+    [SerializeField] ParticleSystem deathPS1;
+    [SerializeField] ParticleSystem deathPS2;
+    [SerializeField] AudioClip collisionSound;
+    [SerializeField] AudioClip deadSound;
+    [SerializeField] AudioClip engineSound;
+    [SerializeField] AudioClip powerUpSound;
+    [SerializeField] AudioClip finishSound;
+
+    private AudioSource audioSource1;
+    private AudioSource audioSource2;
+    private enum State { Transcending, Alive, Dying, Damaging };
+    private State state = State.Alive;
+    private PlayerHealth playerHealth;
+
+	void Start () {
+        audioSource1 = GetComponents<AudioSource>()[0];
+        audioSource2 = GetComponents<AudioSource>()[1];
+        playerHealth = GetComponent<PlayerHealth>();
+	}
+	
+	void Update () {
+        if (playerHealth.currentHealth < 100) deathPS2.Play();
+        else deathPS2.Stop();
+
+        if (state == State.Alive || state == State.Damaging)
+        {
+            Thrust();
+            Rotate();
+        }
+	}
+
+    private void Thrust(){
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce
+                     (Vector3.up * thrustPowerPerFrame);
+            if (!audioSource1.isPlaying) audioSource2.PlayOneShot(engineSound);
+            enginePS.Play();
+        } else
+        {
+            audioSource2.Stop();
+            enginePS.Stop();
+        }
+    }
+
+    private void Rotate(){
+        //rigidbody.freezeRotation = true;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate
+                     (Vector3.back * rotatePowerPerFrame * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate
+                     (Vector3.forward * rotatePowerPerFrame * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state == State.Alive)
+        {
+            GameObject colObj = collision.gameObject;
+            switch (colObj.tag)
+            {
+                case "Friendly":
+                    break;
+                case "Enemy":
+                    if (state != State.Damaging)
+                    {
+                        if (playerHealth.currentHealth >= damage)
+                        {
+                            state = State.Damaging;
+                            damagedPS.Play();
+                            playerHealth.TakeDamage(damage);
+                            audioSource1.PlayOneShot(collisionSound);
+                            Invoke("ResetAlive", collisionSound.length);
+                        }
+                        else
+                        {
+                            deathPS1.Play();
+                            audioSource1.Stop();
+                            audioSource1.PlayOneShot(deadSound);
+                            enginePS.Stop();
+                            Invoke("ifDie", deadSound.length + transitionTime);
+                            state = State.Dying;
+                        }
+                    }
+                    break;
+                case "Fuel":
+                    audioSource1.PlayOneShot(powerUpSound);
+                    ifPowerUp();
+                    break;
+                case "Finish":
+                    audioSource1.Stop();
+                    successPS.Play();
+                    audioSource1.PlayOneShot(finishSound);
+                    Invoke("ifFinish", finishSound.length + transitionTime);
+                    state = State.Transcending;
+                    break;
+            }
+        }
+    }
+
+    private void ifDie(){
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    private void ifFinish(){
+        currentLevel += 1;
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    private void ifPowerUp(){
+        
+    }
+
+    private void ResetAlive(){
+        state = State.Alive;
+    }
+}
---
+++ Advanced/RocketAdventure/Assets/UI/TimeSlider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSlider : MonoBehaviour {
+
+    public float levelSeconds;
+
+    private Slider slider;
+    private float startTime;
+    private LevelManager levelManager;
+
+	void Start () {
+        slider = GetComponent<Slider>();
+        levelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timeLeft = (Time.time - startTime) / levelSeconds;
+        slider.value = timeLeft;
+        print("TimeSlider1 = " + timeLeft.ToString());
+        print("TimeSlider2 = " + slider.value.ToString());
+
+        bool timeIsUp = ((Time.time - startTime) >= levelSeconds);
+        if (timeIsUp)
+        {
+            // levelManager.LoadNextLevel(); // TODO change to fuel
+
+        }
+	}
+
+    public void StartTime(){
+        slider = GetComponent<Slider>();
+        startTime = Time.time;
+        gameObject.SetActive(true);
+    }
+}
